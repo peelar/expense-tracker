d8c3daf68f3a108059d07059845c7fc4dcb398fd
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2021-11-14 10:26:12 +0100

    feat: :sparkles: add animation & env vars for sheets

--- src/components/Expense.tsx
+++ src/components/Expense.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React from "react"
+import { motion } from "framer-motion"
 
-import { ExpenseForm } from "./ExpenseForm";
+import { ExpenseForm } from "./ExpenseForm"
 
 export const Expense = () => {
-  return (
-    <div className="w-full h-full flex items-center justify-center">
-      <section className="ring-4 ring-green-400 rounded flex flex-col h-3/6 w-full max-w-md bg-white shadow-xl">
-        <div className="w-full flex justify-center px-4 py-4 bg-green-400 text-white border-b-2 border-green-200">
-          <span className="font-bold text-lg text-center">Expense</span>
-        </div>
-        <div className="px-4 py-4 h-full">
-          <ExpenseForm />
-        </div>
-      </section>
-    </div>
-  );
-};
+    return (
+        <motion.div
+            className="w-full h-full flex items-center justify-center"
+            animate={{ scale: [0.9, 1], opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}>
+            <section className="ring-4 ring-green-400 rounded flex flex-col w-full max-w-md bg-white shadow-xl">
+                <div className="w-full flex justify-center px-4 py-4 bg-green-400 text-white border-b-2 border-green-200">
+                    <span className="font-bold text-lg text-center">Expense</span>
+                </div>
+                <div className="px-4 py-4 h-full">
+                    <ExpenseForm />
+                </div>
+            </section>
+        </motion.div>
+    )
+}
--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,107 +1,98 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
-import { useMutation } from "react-query";
+import React from "react"
+import { useForm } from "react-hook-form"
+import toast from "react-hot-toast"
+import { useMutation } from "react-query"
 
-import { useCategories } from "../hooks/useCategories";
-import { Button } from "./Button";
-import { Field } from "./Field";
+import { useCategories } from "../hooks/useCategories"
+import { Button } from "./Button"
+import { Field } from "./Field"
 
 export type ExpenseFormFields = {
-  category: string;
-  amount: number;
-  date: Date;
-  description: string;
-};
+    category: string
+    amount: number
+    date: Date
+    description: string
+}
 
 export const ExpenseForm = () => {
-  const { data } = useCategories();
-  const [count, setCount] = React.useState(0);
+    const { data } = useCategories()
+    const [count, setCount] = React.useState(0)
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { isValid },
-  } = useForm<ExpenseFormFields>({
-    mode: "onChange",
-  });
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { isValid },
+    } = useForm<ExpenseFormFields>({
+        mode: "onChange",
+    })
 
-  const { isLoading, mutate } = useMutation(
-    "expense",
-    (values: ExpenseFormFields) =>
-      fetch("/api/expense", {
-        method: "POST",
-        body: JSON.stringify(values),
-      }).then((res) => res.json()),
-    {
-      onSuccess: () => {
-        const nextCount = count + 1;
-        setCount(nextCount);
-        toast.success(
-          nextCount === 5 ? "Wow! 5 expenses in a row!" : "Added an expense! 🎉"
-        );
-      },
-      onError: () => {
-        toast.error("Something went wrong 😢");
-      },
+    const { isLoading, mutate } = useMutation(
+        "expense",
+        (values: ExpenseFormFields) =>
+            fetch("/api/expense", {
+                method: "POST",
+                body: JSON.stringify(values),
+            }).then(res => res.json()),
+        {
+            onSuccess: () => {
+                const nextCount = count + 1
+                setCount(nextCount)
+                toast.success(nextCount === 5 ? "Wow! 5 expenses in a row!" : "Added an expense! 🎉")
+            },
+            onError: () => {
+                toast.error("Something went wrong 😢")
+            },
+        }
+    )
+
+    const onSubmit = (data: ExpenseFormFields) => {
+        mutate(data)
     }
-  );
 
-  const onSubmit = (data: ExpenseFormFields) => {
-    mutate(data);
-  };
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-10 h-full">
+            <div className="flex flex-col gap-4">
+                <Field label="Amount" required>
+                    <input
+                        {...register("amount", { required: true })}
+                        className="text-black py-4"
+                        type="number"
+                        placeholder="1$"
+                    />
+                </Field>
+                <Field label="Date">
+                    <input {...register("date")} className="text-black py-4" type="date" />
+                </Field>
 
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col justify-between h-full"
-    >
-      <div className="flex flex-col gap-4">
-        <Field label="Amount" required>
-          <input
-            {...register("amount", { required: true })}
-            className="text-black py-4"
-            type="number"
-            placeholder="1$"
-          />
-        </Field>
-        <Field label="Date">
-          <input
-            {...register("date")}
-            className="text-black py-4"
-            type="date"
-          />
-        </Field>
-
-        <Field label="Category">
-          <select {...register("category")} className="text-black py-4">
-            {data?.values?.map(([id, category]) => (
-              <option key={id} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </Field>
-        <Field label="Description">
-          <input
-            {...register("description")}
-            className="text-black py-4"
-            type="text"
-            placeholder="A cupcake"
-          />
-        </Field>
-      </div>
-      <div className="grid grid-cols-4 gap-2">
-        <div className="col-span-3">
-          <Button disabled={!isValid} className="py-4 w-full">
-            {isLoading ? "Loading..." : "Add"}
-          </Button>
-        </div>
-        <Button onClick={() => reset()} variant="secondary">
-          Reset
-        </Button>
-      </div>
-    </form>
-  );
-};
+                <Field label="Category">
+                    <select {...register("category")} className="text-black py-4">
+                        {data?.values?.map(([id, category]) => (
+                            <option key={id} value={category}>
+                                {category}
+                            </option>
+                        ))}
+                    </select>
+                </Field>
+                <Field label="Description">
+                    <input
+                        {...register("description")}
+                        className="text-black py-4"
+                        type="text"
+                        placeholder="A cupcake"
+                    />
+                </Field>
+            </div>
+            <div className="grid grid-cols-4 gap-2">
+                <div className="col-span-3">
+                    <Button disabled={!isValid} className="py-4 w-full">
+                        {isLoading ? "Loading..." : "Add"}
+                    </Button>
+                </div>
+                <Button onClick={() => reset()} variant="secondary">
+                    Reset
+                </Button>
+            </div>
+        </form>
+    )
+}
